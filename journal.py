dd7b075dd9f31ea8ae5cd6092df7a885e098b467
farhatzain <zain.farhat@programi.tech>
2025-06-24 16:40:04 +0300

    Analyzing student performance

--- journal.py
+++ journal.py
@@ -46,3 +46,66 @@ for student in class_journal:
   print("Grades:", grades)
   print("Average of " + student + ": " + str(average))
   print("-------------") 
+
+#Part 3: Deeper Analysis
+
+highest_avg = 0 # This will starts with 0 untill we find the highest average.
+highest_avg_student = "" # An empty string to save the name of the student with the highest average .
+
+
+smallest_range = 100  # Starting value.
+most_consistent_student = ""
+
+students_with_low_grade = []
+
+total_grades = 0
+sum_of_all_grades = 0
+
+# pass by each student
+for name in class_journal:
+  grades = class_journal[name]  # grab their grades
+  total = 0
+
+  for grade in grades:
+    total += grade
+
+  average = total / len(grades) 
+
+  if average > highest_avg:
+    highest_avg = average
+    highest_avg_student = name  # Student having the highest Average
+
+# Most consistent performance 
+  grade_range = max(grades) - min(grades)  
+  if grade_range < smallest_range:
+    smallest_range = grade_range
+    most_consistent_student = name
+
+  # list of students with any grade below 70
+  for grade in grades:
+    if grade < 70: 
+      if name not in students_with_low_grade:
+        students_with_low_grade.append(name)
+  
+  total_grades += len(grades)
+  sum_of_all_grades += total  
+
+# Overall Class Average
+class_average = round(sum_of_all_grades / total_grades, 2)
+
+# Display the results results
+print("Student with the highest average is:", highest_avg_student, "with", round(highest_avg, 2))
+print("Most consistent performance :", most_consistent_student, "with range of", smallest_range)
+print("Students with a grade < 70:", students_with_low_grade)
+print("Total number of grades:", total_grades)
+print("Overall class average is:", class_average)
+
+# Save to file
+file = open("class_analysis_report.txt", "w")
+file.write("Student with the highest average is: " + highest_avg_student + " having " + str(round(highest_avg, 2)) + "\n")
+file.write("Most consistent performance :" + most_consistent_student + " with range of " + str(smallest_range) + "\n")
+file.write("Students with a grade < 70:" + ", ".join(students_with_low_grade) + "\n")
+file.write("Total number of grades: " + str(total_grades) + "\n")
+file.write("Overall class average is " + str(class_average) + "\n")
+file.close()
+print("Check text file  'Class_Report.txt'")
